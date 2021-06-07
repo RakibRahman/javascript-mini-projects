b0cf934faacf337ab0fcd11d57e84ecfa8ed3945
RakibRahman <md.rr.talukder@gmail.com>
2021-06-07 18:02:23 +0600

    Modal updated

--- modal/app.js
+++ modal/app.js
@@ -1,6 +1,8 @@
 const modal = document.querySelector("#modal");
 const closeBtn = document.querySelector("#close");
 const subscribe = document.querySelector("#subs");
+const main = document.querySelector("#main");
+console.log(main);
 
 function toggleModal() {
   if (modal.classList.contains("hidden")) {
@@ -12,5 +14,12 @@ function toggleModal() {
 function closeModal() {
   modal.classList.add("hidden");
 }
+function clickAnywhere(e) {
+  console.log(e.target);
+  if (e.target === main) {
+    modal.classList.add("hidden");
+  }
+}
 subscribe.addEventListener("click", toggleModal);
 closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", clickAnywhere);
